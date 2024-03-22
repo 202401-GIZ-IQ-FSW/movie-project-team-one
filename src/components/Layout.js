a171ef4b7612510ec5531829e76de164b4f09594
Nada-123 <nada.alshakiry@gmail.com>
2024-03-22 16:05:53 +0300

    Added Nabar Folder in Components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,10 @@
 import React from "react";
-import Navbar from "./Navbar";
+import Navbar from "./Navbar/Navbar";
 
 const Layout = ({ children }) => {
 	return (
 		<div>
-			<Navbar/>
+			<Navbar />
 			{children}
 		</div>
 	);
