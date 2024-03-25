108a575d80b80dadf1dc2f5b8abb99f0aed018bb
Didam Goran <didamgoran01@gmail.com>
2024-03-26 00:57:50 +0300

    Implementing burger menu for mibile view

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,102 +1,133 @@
 "use client";
-import React, { useState } from "react";
-import NavbarItem from "./NavItems";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import NavbarItem from "./NavItems";
 
 export default function Navbar() {
-	const [isOpen, setIsOpen] = useState(false);
-	const user = null;
+  const [isOpen, setIsOpen] = useState(false);
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+  const user = null;
 
-	const toggleDropdown = () => {
-		setIsOpen(!isOpen);
-	};
+  const toggleMoviesDropdown = () => {
+    setIsOpen(!isOpen);
+  };
 
-	return (
-		<div className='bg-slate-700 sticky z-50 top-0 flex h-16 items-center text-[#FDAE0D] w-screen px-16 justify-between'>
-			{/* The app Icon */}
-			<div className='ml-4 flex lg:ml-0'>
-				<Image
-					src='/img/logo/logo.png'
-					alt='Logo'
-					width={96}
-					height={96}
-				/>
-			</div>
+  const toggleNavbar = () => {
+    setIsNavbarOpen(!isNavbarOpen);
+  };
 
-			{/* Nav items section  */}
-			<div className='justify-between w-full hidden md:flex'>
-				<div className='ml-auto flex items-center'>
-					<Link
-						href={{ pathname: "/" }}
-						className='block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105'
-					>
-						Home
-					</Link>
+  return (
+    <div className="bg-slate-700 sticky z-50 top-0 flex h-16 items-center text-[#FDAE0D] w-screen px-16 justify-between">
+      {/* The app Icon */}
+      <div className="ml-4 flex lg:ml-0">
+        <Image src="/img/logo/logo.png" alt="Logo" width={96} height={96} />
+      </div>
 
-					<div className='relative'>
-						<button
-							onClick={toggleDropdown}
-							className='px-4 py-2 hover:text-gray-200 hover:animate-pulse hover:scale-105'
-						>
-							Movies
-						</button>
-						{isOpen && (
-							<div className='absolute -right-10 mt-2 bg-slate-700  text-white rounded shadow-lg flex flex-col p-3 gap-2 w-40'>
-								<NavbarItem
-									title='Popular'
-									param='popular'
-								/>
-								<NavbarItem
-									title='Top Rated'
-									param='top_rated'
-								/>
-								<NavbarItem
-									title='Now Playing'
-									param='now_playing'
-								/>
-								<NavbarItem
-									title='Upcoming'
-									param='upcoming'
-								/>
-							</div>
-						)}
-					</div>
+      {/* Mobile menu icon */}
+      <div>
+        <button
+          onClick={toggleNavbar}
+          className="md:hidden px-4 py-2 text-white">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16m-7 6h7"
+            />
+          </svg>
+        </button>
+        {isNavbarOpen && (
+          <div className="absolute -right-10 mt-2 bg-slate-700  text-white rounded shadow-lg flex flex-col p-3 gap-2 w-40">
+            <Link
+              href={{ pathname: "/" }}
+              className="block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105">
+              Home
+            </Link>
+            {/* Can you add the movies section as well */}
+            <div className="relative">
+              <button
+                onClick={toggleMoviesDropdown}
+                className="px-4 py-2 hover:text-gray-200 hover:animate-pulse hover:scale-105">
+                Movies
+              </button>
+              {isOpen && (
+                <div className="absolute -right-10 mt-2 bg-slate-700  text-white rounded shadow-lg flex flex-col p-3 gap-2 w-40">
+                  <NavbarItem title="Popular" param="popular" />
+                  <NavbarItem title="Top Rated" param="top_rated" />
+                  <NavbarItem title="Now Playing" param="now_playing" />
+                  <NavbarItem title="Upcoming" param="upcoming" />
+                </div>
+              )}
+            </div>
+            <Link
+              href={{ pathname: "/actors/page" }}
+              className="block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105">
+              Actors
+            </Link>
+            <Link
+              href={{ pathname: "/about/page" }}
+              className="block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105">
+              About
+            </Link>
+          </div>
+        )}
+      </div>
 
-					<Link
-						href={{ pathname: "/actors/page" }}
-						className='block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105'
-					>
-						Actors
-					</Link>
+      {/* Nav items section  */}
+      <div className="justify-between w-full hidden md:flex">
+        <div className="ml-auto flex items-center">
+          <Link
+            href={{ pathname: "/" }}
+            className="block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105">
+            Home
+          </Link>
 
-					<Link
-						href={{ pathname: "/about/page" }}
-						className='block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105'
-					>
-						About
-					</Link>
-				</div>
-				{/* Sign in and Sign Up Nav Section */}
-				<div className='ml-auto flex items-center'>
-					<div className='hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6'>
-						{user ? null : (
-							<Link href='/sign-in'>Sign in</Link>
-						)}
-						{user ? null : (
-							<span
-								className='h-6 w-px bg-gray-200'
-								aria-hidden='true'
-							/>
-						)}
-						{user ? (
-							<p></p>
-						) : (
-							<Link href='/sign-up'>Sign up</Link>
-						)}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+          <div className="relative">
+            <button
+              onClick={toggleMoviesDropdown}
+              className="px-4 py-2 hover:text-gray-200 hover:animate-pulse hover:scale-105">
+              Movies
+            </button>
+            {isOpen && (
+              <div className="absolute -right-10 mt-2 bg-slate-700  text-white rounded shadow-lg flex flex-col p-3 gap-2 w-40">
+                <NavbarItem title="Popular" param="popular" />
+                <NavbarItem title="Top Rated" param="top_rated" />
+                <NavbarItem title="Now Playing" param="now_playing" />
+                <NavbarItem title="Upcoming" param="upcoming" />
+              </div>
+            )}
+          </div>
+
+          <Link
+            href={{ pathname: "/actors/page" }}
+            className="block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105">
+            Actors
+          </Link>
+
+          <Link
+            href={{ pathname: "/about/page" }}
+            className="block px-4 py-2  hover:text-gray-200 hover:animate-pulse hover:scale-105">
+            About
+          </Link>
+        </div>
+        {/* Sign in and Sign Up Nav Section */}
+        <div className="ml-auto flex items-center">
+          <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+            {user ? null : <Link href="/sign-in">Sign in</Link>}
+            {user ? null : (
+              <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+            )}
+            {user ? <p></p> : <Link href="/sign-up">Sign up</Link>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,119 +1,296 @@
+// import React from "react";
+// import Image from "next/image";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+// 	faFacebookF,
+// 	faTwitter,
+// 	faPinterestP,
+// 	faLinkedinIn,
+// } from "@fortawesome/free-brands-svg-icons";
+
+// function Footer() {
+// 	return (
+// 		<footer className='bg-gray-800 text-white'>
+// 			<div className='container mx-auto py-8'>
+// 				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+// 					<div className='col-span-1 md:col-span-2'>
+// 						<div className='footer-logo pl-2'>
+// 							<a href='index.html'>
+// 								<Image
+// 									src='/img/logo/logo.png'
+// 									alt='Logo'
+// 									width={100}
+// 									height={30}
+// 								/>
+// 							</a>
+// 						</div>
+// 					</div>
+// 					<div className='col-span-1 md:col-span-1 lg:text-right pr-4'>
+// 						<nav className='footer-menu'>
+// 							<ul className='flex justify-center md:justify-end space-x-6'>
+// 								<li>
+// 									<a href='index.html'>HOME</a>
+// 								</li>
+// 								<li>
+// 									<a href='index.html'>MOVIE</a>
+// 								</li>
+// 								<li>
+// 									<a href='index.html'>TV SHOW</a>
+// 								</li>
+// 								<li>
+// 									<a href='index.html'>PAGES</a>
+// 								</li>
+// 								<li>
+// 									<a href='pricing.html'>PRICING</a>
+// 								</li>
+// 							</ul>
+// 						</nav>
+// 					</div>
+// 					<div className='lg:flex lg:justify-between lg:items-center pl-4'>
+// 						<div className='lg:text-left'>
+// 							<div className='quick-link-list'>
+// 								<ul className='flex justify-center md:justify-start space-x-6'>
+// 									<li>
+// 										<a href='#'>FAQ</a>
+// 									</li>
+// 									<li>
+// 										<a href='#'>HELP CENTER</a>
+// 									</li>
+// 									<li>
+// 										<a href='#'>TERMS OF USE</a>
+// 									</li>
+// 									<li>
+// 										<a href='#'>PRIVACY</a>
+// 									</li>
+// 								</ul>
+// 							</div>
+// 						</div>
+// 						<div className='lg:text-right'>
+// 							<div className='footer-social flex justify-center md:justify-end space-x-4'>
+// 								<a
+// 									href='#'
+// 									className='text-gray-400 hover:text-gray-500'
+// 								>
+// 									<FontAwesomeIcon icon={faFacebookF} />
+// 								</a>
+// 								<a
+// 									href='#'
+// 									className='text-gray-400 hover:text-gray-500'
+// 								>
+// 									<FontAwesomeIcon icon={faTwitter} />
+// 								</a>
+// 								<a
+// 									href='#'
+// 									className='text-gray-400 hover:text-gray-500'
+// 								>
+// 									<FontAwesomeIcon icon={faPinterestP} />
+// 								</a>
+// 								<a
+// 									href='#'
+// 									className='text-gray-400 hover:text-gray-500'
+// 								>
+// 									<FontAwesomeIcon icon={faLinkedinIn} />
+// 								</a>
+// 							</div>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 			<div className='bg-gray-900 py-4 pl-4'>
+// 				<div className='container mx-auto'>
+// 					<div className='text-center md:text-left'>
+// 						<p>
+// 							&copy; {new Date().getFullYear()}. All Rights
+// 							Reserved By{" "}
+// 							<a
+// 								href='index.html'
+// 								className='text-yellow-300'
+// 							>
+// 								Team One
+// 							</a>
+// 						</p>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</footer>
+// 	);
+// }
+
+// export default Footer;
+
+
+
+
+
+
+// import React from "react";
+// import Image from "next/image";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faFacebookF,
+//   faTwitter,
+//   faPinterestP,
+//   faLinkedinIn,
+// } from "@fortawesome/free-brands-svg-icons";
+
+// function Footer() {
+//   return (
+//     <footer className="bg-gray-800 text-white">
+//       <div className="container mx-auto py-8">
+//         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+//           {/* Logo Section */}
+//           <div className="flex justify-center items-center md:col-span-1">
+//             <div className="footer-logo pl-4">
+//               <a href="index.html">
+//                 <Image
+//                   src="/img/logo/logo.png"
+//                   alt="Logo"
+//                   width={100}
+//                   height={30}
+//                 />
+//               </a>
+//             </div>
+//           </div>
+
+//           {/* Middle Section for Links */}
+//           <div className="col-span-1 md:col-span-1 lg:text-center">
+//             <div className="quick-link-list">
+//               <ul className="flex justify-center md:justify-start space-x-6">
+//                 <li>
+//                   <a href="#">FAQ</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">HELP CENTER</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">TERMS OF USE</a>
+//                 </li>
+//                 <li>
+//                   <a href="#">PRIVACY</a>
+//                 </li>
+//               </ul>
+//             </div>
+//           </div>
+
+//           {/* Right Section for Home and Movie Links */}
+//           <div className="flex justify-center items-center md:col-span-1">
+//             <nav className="footer-menu">
+//               <ul className="flex justify-center md:justify-end space-x-6">
+//                 <li>
+//                   <a href="index.html">HOME</a>
+//                 </li>
+//                 <li>
+//                   <a href="index.html">MOVIE</a>
+//                 </li>
+//                 {/* <li>
+//                   <a href="index.html">TV SHOW</a>
+//                 </li> */}
+//               </ul>
+//             </nav>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="bg-gray-900 py-4 pl-4">
+//         <div className="container mx-auto">
+//           <div className="text-center md:text-left">
+//             <p>
+//               &copy; {new Date().getFullYear()}. All Rights Reserved By{" "}
+//               <a href="index.html" className="text-yellow-300">
+//                 Team One
+//               </a>
+//             </p>
+//           </div>
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+
 import React from "react";
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-	faFacebookF,
-	faTwitter,
-	faPinterestP,
-	faLinkedinIn,
+  faFacebookF,
+  faTwitter,
+  faPinterestP,
+  faLinkedinIn,
 } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
-	return (
-		<footer className='bg-gray-800 text-white'>
-			<div className='container mx-auto py-8'>
-				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-					<div className='col-span-1 md:col-span-2'>
-						<div className='footer-logo pl-2'>
-							<a href='index.html'>
-								<Image
-									src='/img/logo/logo.png'
-									alt='Logo'
-									width={100}
-									height={30}
-								/>
-							</a>
-						</div>
-					</div>
-					<div className='col-span-1 md:col-span-1 lg:text-right pr-4'>
-						<nav className='footer-menu'>
-							<ul className='flex justify-center md:justify-end space-x-6'>
-								<li>
-									<a href='index.html'>HOME</a>
-								</li>
-								<li>
-									<a href='index.html'>MOVIE</a>
-								</li>
-								<li>
-									<a href='index.html'>TV SHOW</a>
-								</li>
-								<li>
-									<a href='index.html'>PAGES</a>
-								</li>
-								<li>
-									<a href='pricing.html'>PRICING</a>
-								</li>
-							</ul>
-						</nav>
-					</div>
-					<div className='lg:flex lg:justify-between lg:items-center pl-4'>
-						<div className='lg:text-left'>
-							<div className='quick-link-list'>
-								<ul className='flex justify-center md:justify-start space-x-6'>
-									<li>
-										<a href='#'>FAQ</a>
-									</li>
-									<li>
-										<a href='#'>HELP CENTER</a>
-									</li>
-									<li>
-										<a href='#'>TERMS OF USE</a>
-									</li>
-									<li>
-										<a href='#'>PRIVACY</a>
-									</li>
-								</ul>
-							</div>
-						</div>
-						<div className='lg:text-right'>
-							<div className='footer-social flex justify-center md:justify-end space-x-4'>
-								<a
-									href='#'
-									className='text-gray-400 hover:text-gray-500'
-								>
-									<FontAwesomeIcon icon={faFacebookF} />
-								</a>
-								<a
-									href='#'
-									className='text-gray-400 hover:text-gray-500'
-								>
-									<FontAwesomeIcon icon={faTwitter} />
-								</a>
-								<a
-									href='#'
-									className='text-gray-400 hover:text-gray-500'
-								>
-									<FontAwesomeIcon icon={faPinterestP} />
-								</a>
-								<a
-									href='#'
-									className='text-gray-400 hover:text-gray-500'
-								>
-									<FontAwesomeIcon icon={faLinkedinIn} />
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div className='bg-gray-900 py-4 pl-4'>
-				<div className='container mx-auto'>
-					<div className='text-center md:text-left'>
-						<p>
-							&copy; {new Date().getFullYear()}. All Rights
-							Reserved By{" "}
-							<a
-								href='index.html'
-								className='text-yellow-300'
-							>
-								Team One
-							</a>
-						</p>
-					</div>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="bg-gray-800 text-white">
+      <div className="container mx-auto py-8">
+		
+        <div className="grid grid-cols-3 gap-4">
+          {/* Logo Section */}
+          <div className="col-span-1 md:col-span-1 flex justify-center md:justify-start items-center">
+            <div className="footer-logo pl-4">
+              <a href="index.html">
+                <Image
+                  src="/img/logo/logo.png"
+                  alt="Logo"
+                  width={100}
+                  height={30}
+                />
+              </a>
+            </div>
+          </div>
+
+          {/* Middle Section for Links */}
+          <div className="col-span-1 md:col-span-1 lg:text-center">
+            <div className="quick-link-list">
+              <ul className="flex justify-center md:justify-start space-x-6">
+                <li>
+                  <a href="#">FAQ</a>
+                </li>
+                <li>
+                  <a href="#">HELP CENTER</a>
+                </li>
+                <li>
+                  <a href="#">TERMS OF USE</a>
+                </li>
+                <li>
+                  <a href="#">PRIVACY</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          {/* Right Section for Home and Movie Links */}
+          <div className="col-span-1 md:col-span-1 lg:text-right pr-4">
+            <nav className="footer-menu">
+              <ul className="flex justify-center md:justify-end space-x-6">
+                <li>
+                  <a href="index.html">HOME</a>
+                </li>
+                <li>
+                  <a href="index.html">MOVIE</a>
+                </li>
+                {/* <li>
+                  <a href="index.html">TV SHOW</a>
+                </li> */}
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-900 py-4 pl-4">
+        <div className="container mx-auto">
+          <div className="text-center md:text-left">
+            <p>
+              &copy; {new Date().getFullYear()}. All Rights Reserved By{" "}
+              <a href="index.html" className="text-yellow-300">
+                Team One
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
