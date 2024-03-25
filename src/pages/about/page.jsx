4c49cbdd53e6a12f9bc3a8babb52ee1aacd3ed2f
Nada Al-Shakiry <ddo@Nadas-MacBook-Pro.local>
2024-03-25 22:57:14 +0300

    Added About Page

---
+++ src/pages/about/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from "@/components/Layout";
+import "../../app/globals.css";
+
+function About() {
+  return (
+    <Layout>About</Layout>
+  )
+}
+
+export default About
