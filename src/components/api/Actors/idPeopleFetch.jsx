f61b636a30802d240024cf0b872e71455274388e
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-03-26 13:57:11 +0300

    Fix Error: React Hook "useState" is called in function "idPeopleFetch" that is neither  a React function component nor a custom React Hook function in production.

--- src/components/api/Actors/idPeopleFetch.jsx
+++ src/components/api/Actors/idPeopleFetch.jsx
@@ -3,33 +3,32 @@ import axios from "axios";
 
 const API_KEY = "258db8ec4984148dcc9558ed97d94f6a";
 
-function idPeopleFetch({ actor_id }) {
-	const [actor, setActor] = useState([]);
-	const URL = `https://api.themoviedb.org/3/person/${actor_id}`;
+function useIdPeopleFetch({ actor_id }) {
+    const [actor, setActor] = useState([]);
+    const URL = `https://api.themoviedb.org/3/person/${actor_id}`;
 
-	useEffect(() => {
-		// Check if actor_id is available
-		if (actor_id) {
-			const fetchActor = async () => {
-				try {
-					const response = await axios.get(URL, {
-						params: {
-							api_key: API_KEY,
-							language: "en-US",
-						},
-					});
-					setActor(response.data); // Set the entire actor object
-				} catch (error) {
-					console.error("Error fetching data:", error);
-					setActor(null); // Reset actor state in case of error
-				}
-			};
+    useEffect(() => {
+        if (actor_id) {
+            const fetchActor = async () => {
+                try {
+                    const response = await axios.get(URL, {
+                        params: {
+                            api_key: API_KEY,
+                            language: "en-US",
+                        },
+                    });
+                    setActor(response.data);
+                } catch (error) {
+                    console.error("Error fetching data:", error);
+                    setActor(null);
+                }
+            };
 
-			fetchActor();
-		}
-	}, [actor_id]); // Run the effect only when actor_id changes
+            fetchActor();
+        }
+    }, [actor_id]);
 
-	return actor;
+    return actor;
 }
 
-export default idPeopleFetch;
+export default useIdPeopleFetch;
