dfec216cb9c1951900aa1126f07f5db097cb351c
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-03-26 14:07:11 +0300

    Fix Error: React Hook "useState" is called in function "peopleFetch" that is neither a  React function component nor a custom React Hook function in production

--- src/components/api/Actors/peopleFetch.jsx
+++ src/components/api/Actors/peopleFetch.jsx
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"; // Import Axios
+import axios from "axios";
 
 const API_KEY = "258db8ec4984148dcc9558ed97d94f6a";
 
-function peopleFetch() {
-	const [people, setPeople] = useState([]);
-	const link = `https://api.themoviedb.org/3/person/popular`;
+function usePeopleFetch() {
+    const [people, setPeople] = useState([]);
+    const link = `https://api.themoviedb.org/3/person/popular`;
 
-	useEffect(() => {
-		const fetchPeople = async () => {
-			try {
-				const response = await axios.get(link, {
-					params: {
-						api_key: API_KEY,
-						language: "en-US",
-						page: 1,
-					},
-				});
-				setPeople(response.data.results);
-			} catch (error) {
-				console.error("Error fetching data:", error);
-			}
-		};
+    useEffect(() => {
+        const fetchPeople = async () => {
+            try {
+                const response = await axios.get(link, {
+                    params: {
+                        api_key: API_KEY,
+                        language: "en-US",
+                        page: 1,
+                    },
+                });
+                setPeople(response.data.results);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
 
-		fetchPeople();
-	}, []); // Run the effect only once when the component mounts
+        fetchPeople();
+    }, []);
 
-	return people;
+    return people;
 }
 
-export default peopleFetch;
+export default usePeopleFetch;
