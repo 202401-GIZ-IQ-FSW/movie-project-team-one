1cc7558a6d9773d49214ac935fbe32b302ea78c6
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-03-26 14:20:53 +0300

    Fix Error: React Hook "useState" is called in function "castFetch" that is neither a React function component  nor a custom React Hook function in production.

--- src/components/api/movies/castFetch.jsx
+++ src/components/api/movies/castFetch.jsx
@@ -1,36 +1,36 @@
 import { useState, useEffect } from "react";
-import axios from "axios"; // Import Axios
+import axios from "axios";
 
 const API_KEY = "258db8ec4984148dcc9558ed97d94f6a";
 
-function castFetch({ movie_id }) {
-	const [cast, setCast] = useState([]);
+function useCastFetch({ movie_id }) {
+    const [cast, setCast] = useState([]);
 
-	useEffect(() => {
-		if (movie_id) {
-			const fetchCast = async () => {
-				try {
-					const response = await axios.get(
-						`https://api.themoviedb.org/3/movie/${movie_id}/credits`,
-						{
-							params: {
-								api_key: API_KEY,
-								language: "en-US",
-								page: 1,
-							},
-						}
-					);
-					setCast(response.data.cast); // Set the cast array from response data
-				} catch (error) {
-					console.error("Error fetching cast data:", error);
-				}
-			};
+    useEffect(() => {
+        if (movie_id) {
+            const fetchCast = async () => {
+                try {
+                    const response = await axios.get(
+                        `https://api.themoviedb.org/3/movie/${movie_id}/credits`,
+                        {
+                            params: {
+                                api_key: API_KEY,
+                                language: "en-US",
+                                page: 1,
+                            },
+                        }
+                    );
+                    setCast(response.data.cast);
+                } catch (error) {
+                    console.error("Error fetching cast data:", error);
+                }
+            };
 
-			fetchCast();
-		}
-	}, [movie_id]);
+            fetchCast();
+        }
+    }, [movie_id]);
 
-	return cast;
+    return cast;
 }
 
-export default castFetch;
+export default useCastFetch;
