76587af4ea5284ce9c72d860223c5e2dc7d01cd5
papula-a <pa20381@auis.edu.krd>
2024-03-26 19:53:33 +0300

    picture for about page added

--- src/pages/about/page.jsx
+++ src/pages/about/page.jsx
@@ -40,7 +40,7 @@ function About() {
 
               <div className="bg-[#344156] p-4 rounded-lg flex flex-col items-center">
                 <Image
-                  src="/img/images/episode_img.jpg"
+                  src="/img/about/didam.jpg"
                   alt="Mohammed"
                   width={300}
                   height={200}
